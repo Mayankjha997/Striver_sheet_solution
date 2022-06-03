b498e1065da1b7424a581c3bd16085f63491cbed
Mayank Jha <mayankjha997@gmail.com>
2022-06-04 02:25:50 +0530

    init1

--- string part - 2/compare_version_number.py
+++ string part - 2/compare_version_number.py
@@ -6,8 +6,8 @@ def compare_version_number(version1,version2):
     len2 = len(version2)
     
     for i in range(max(len1,len2)):
-        l1 = version1[i] if i < len1 else 0
-        l2 = version2[i] if i < len2 else 0
+        l1 = int(version1[i]) if i < len1 else 0
+        l2 = int(version2[i]) if i < len2 else 0
         
         if l1 < l2:
             return -1
@@ -15,4 +15,10 @@ def compare_version_number(version1,version2):
             return 1
         
     return 0
-   
+
+
+version1 = input('enter version1 ')
+version2 = input('enter version2 ')
+v = compare_version_number(version1, version2)
+print(v)
+            
