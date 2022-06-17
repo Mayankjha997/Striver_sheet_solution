399e5ed51d68c7f6fc5da2dfee62b6f67543ae68
Mayank Jha <mayankjha997@gmail.com>
2022-06-17 23:42:01 +0530

    init

---
+++ dynamic programming - 2/word_break.py
@@ -0,0 +1,15 @@
+def word_break(s,worddict):
+    pass
+    
+    
+    
+    
+    
+    
+    
+s = input("enter the string to match with the worddict ")
+worddict = list(map(str,input("enter the wordbreak ").strip().split()))
+print(s)
+print(worddict)
+v = word_break(s,worddict)
+print(v)
